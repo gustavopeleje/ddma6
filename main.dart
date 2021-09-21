825dd7021d9d7456f59ae79938861fbd0d8d3da3
gustavopeleje <44271748+gustavopeleje@users.noreply.github.com>
2021-09-21 14:13:28 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exercício 1!',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Exercício 1!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+  String note = 'Hello';
+  void _onPressed (){
+    setState (() {
+     note = 'Jader';
+    });
+  }
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Exercício 1!',
+            ),
+            Text(
+              '$note',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended (
+        onPressed: _onPressed,
+        tooltip: 'Jader',
+        label: Text('Clique aqui'),   
+      ),
+    );
+  }
+}
